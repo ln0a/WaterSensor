5be0f649e45ab655bd5d315d9d80f3c7a39a8071
Ed Lewis <info@edlewis.co>
2023-04-03 17:56:39 +0100

    video funtions optimised

--- src/rfid.py
+++ src/rfid.py
@@ -8,8 +8,7 @@ import led
 sample_tags = {
     632153281131: 'videos/algae.mp4', 
     975930237824: 'videos/clean.mp4', 
-    842353281731: 'videos/pollution.mp4'
-    }
+    842353281731: 'videos/pollution.mp4'}
 
 # Read RFID tag and lookup tags
 def read(reader):
@@ -22,7 +21,7 @@ def lookup_rfid_tag(id):
     if id in sample_tags:
         # print(sample_tags[id])
         t1 = threading.Thread(target=led.rotate, args=(0, 0, 255, 0.03,))
-        t2 = threading.Thread(target=video.play, args=(sample_tags[id],))
+        t2 = threading.Thread(target=video.play_wait_close, args=(sample_tags[id],))
 
         t1.start()
         t2.start()
--- src/video.py
+++ src/video.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python
 
+import time
 import vlc
 
-def play(file):
-    media = vlc.MediaPlayer(file)
-    media.play()
+def play(source):
+    vlc_instance = vlc.Instance()
+    player = vlc_instance.media_player_new()
+    media = vlc_instance.media_new(source)
+    player.set_media(media)
+    player.play()
+    
+def play_wait_close(source):
+    play(source)
+
+    time.sleep(1.5)
+    duration = player.get_length() / 1000
+    time.sleep(duration)
+
+    print("Video finished")
+    # play_blank()
+
+def play_blank():
+    play("videos/block.mp4")
