89fdc73f654964ac20ed77131825fd4b54e55d62
Ed Lewis <info@edlewis.co>
2023-04-03 17:10:44 +0100

    Added multithreading to run led and video

--- src/rfid.py
+++ src/rfid.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
+import threading
 import video
+import led
 
 # Water sample RFID tag labels
 sample_tags = {
@@ -18,7 +20,14 @@ def read(reader):
 # and play associated video file
 def lookup_rfid_tag(id):
     if id in sample_tags:
-        print(sample_tags[id])
-        video.play(sample_tags[id])
+        # print(sample_tags[id])
+        t1 = threading.Thread(target=led.rotate, args=(0, 0, 255, 0.03,))
+        t2 = threading.Thread(target=video.play, args=(sample_tags[id],))
+
+        t1.start()
+        t2.start()
+
+        t1.join()
+        t2.join()
     else:
         print('No tag found')
