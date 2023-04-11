5a6fcdb6eeaa8f1bc9e775471fc34bb7d6df2613
Ed Lewis <info@edlewis.co>
2023-04-11 11:06:58 +0100

    Cleaned up imports

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 
-import time
+from time import sleep
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 import rfid
+import threading
+import video
+import led
+
 
 # Hide all GPIO warnings
 GPIO.setwarnings(False)
@@ -15,7 +19,15 @@ reader = SimpleMFRC522()
 # Sensor reading loop
 try:
     while True:
-        rfid.read(reader)
+        t1 = threading.Thread(target=led.rotate, args=(0, 0, 255, 0.1,))
+        t2 = threading.Thread(target=rfid.read, args=(reader,))
+
+        t1.start()
+        t2.start()
+
+        t1.join()
+        t2.join()
+
         time.sleep(2)
 except KeyboardInterrupt:
     GPIO.cleanup()
