5b7054b14fe2e72e33e5a841c48851b208ea947a
Ed Lewis <info@edlewis.co>
2023-04-12 17:34:39 +0100

    Logic separated

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ import rfid
 from threading import Thread
 from video import VLC
 from led import LED
+import vlc
 
 
 # Hide all GPIO warnings
@@ -21,9 +22,8 @@ player = VLC()
 led = LED()
 
 
-tread_led = Thread(target = led.rotate)
-tread_led.start()
-
+thread_led = Thread(target = led.rotate)
+thread_led.start()
 
 # Sensor reading loop
 try:
@@ -31,7 +31,16 @@ try:
         player.addVideo("videos/pollution.mp4")
         player.play()
 
-        rfid.read(reader, player, led)
+        if rfid.read(reader):
+            led.speed_change(0.02)
+            led.color_change((0, 255, 0))
+
+            player.addVideo(rfid.read(reader))
+            player.play()
+            player.wait_stop()
+
+            led.speed_reset()
+            led.color_reset()
 
 except KeyboardInterrupt:
     thread_led.join()
--- src/rfid.py
+++ src/rfid.py
@@ -8,38 +8,29 @@ import led
 
 # Water sample RFID tag labels
 sample_tags = {
-    632153281131: 'videos/algae.mp4',
-    975930237824: 'videos/clean.mp4',
-    842353281731: 'videos/pollution.mp4'}
+    632153281131: "videos/algae.mp4",
+    975930237824: "videos/clean.mp4",
+    842353281731: "videos/pollution.mp4"}
 
 
 class RFID(object):
 
     # Read RFID tag and lookup tags
-    def read(self, reader, player, led):
+    def read(self, reader):
         id, label = reader.read()
 
         if self.lookup_rfid_tag(id):
             self.print_tag(id)
-
-            led.speed_change(0.03)
-            led.color_change((0, 255, 0))
-
-            player.addVideo(sample_tags[id])
-            player.play()
-            player.wait_stop()
-
-            led.speed_reset()
-            led.color_reset()
+            return sample_tags[id]
 
     # Check if tag id is stored in tag dictionary
     # and play associated video file
     def lookup_rfid_tag(self, tag):
         if tag in sample_tags:
             return True
-        else:
-            print('No tag found')
-            return False
+        # else:
+        #     print('No tag found')
+        #     return False
 
     def print_tag(self, id):
         print("")
