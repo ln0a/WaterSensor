ff001dd7b62b7114de1c1bb073e801b7b1fbc630
Ed Lewis <info@edlewis.co>
2023-04-12 11:22:29 +0100

    Formatting

--- src/video.py
+++ src/video.py
@@ -5,6 +5,7 @@ import vlc
 
 
 class VLC:
+
     def __init__(self):
         self.Instance = vlc.Instance()
         self.Player = self.Instance.media_player_new()
@@ -13,16 +14,21 @@ class VLC:
     def addVideo(self, source):
         self.Media = self.Instance.media_new(source)
         self.Player.set_media(self.Media)
+
     def play(self):
         self.Player.play()
+
     def next(self):
         self.Player.next()
+
     def previous(self):
         self.Player.previous()
+
     def wait_stop(self):
         time.sleep(2)
         duration = self.Player.get_length() / 1000
         time.sleep(duration)
         self.Player.stop()
+
     def stop(self):
         self.Player.stop()
