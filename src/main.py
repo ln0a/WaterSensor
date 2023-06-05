b539727e13d6dc97e669bc34df45ed9c4aa28c27
Ed Lewis <ed@superflux.in>
2023-06-05 17:53:42 +0100

    Refactoring for the change in vlc use

--- src/main.py
+++ src/main.py
@@ -19,22 +19,15 @@ reader = SimpleMFRC522()
 
 # Initialise control objects
 rfid = rfid.RFID()
-player = VLC()
 led = LED()
 
 thread_led = Thread(target=led.rotate)
 thread_led.start()
 
-# videoPath = "cvlc ~/WaterSensor/videos/Hold.mp4"
-# thread_video = Thread(target=os.popen(videoPath))
-# thread_video.start()
-
 
 # Sensor reading loop
 try:
     while True:
-        # player.addVideo("videos/Hold.mp4")
-        # player.loop()
         os.popen("killall vlc")
         time.sleep(2)
         os.popen("cvlc --loop --fullscreen ~/WaterSensor/videos/Hold.mp4")
@@ -43,14 +36,11 @@ try:
             led.speed_change(0.02)
             led.color_change((0, 255, 0))
 
-            # player.addVideo(rfid.read(reader))
-            # player.play()
-            # player.wait_stop()
             os.popen("killall vlc")
             time.sleep(2)
             os.popen("cvlc --fullscreen ~/WaterSensor/videos/Flooding60.mp4")
 
-            time.sleep(10)
+            time.sleep(20)
             
             led.speed_reset()
             led.color_reset()
