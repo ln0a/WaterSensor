2cec45a7ff5795aebdb23067a5433cb1686deaca
Ed Lewis <ed@superflux.in>
2023-06-08 11:29:48 +0100

    Change to label and time definitiion

--- src/video.py
+++ src/video.py
@@ -5,6 +5,6 @@ class Video(object):
 
     def __init__(self):
         self.files = {
-            632153281131: "flooding",
-            975930237824: "pollution"
+            "sewage": 295,
+            "pollution": 20
         }
